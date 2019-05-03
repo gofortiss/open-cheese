32601765b9a7fd0ae45a2aa26c6bd575d958993e
CPLN <super90xd1@gmail.com>
2019-05-03 14:45:03 +0200

    Creation of connection view

---
+++ application/views/connexion-view.php
@@ -0,0 +1,17 @@
+<div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-4">Connexion</h1>
+  </div>
+</div>
+<form class="connexion" action="Connexion" method="post">
+  <div class="form-group">
+    <label>Pseudo</label>
+    <input type="text" class="form-control" name="pseudo" placeholder="Nom d'utilisateur">
+  </div>
+  <div class="form-group">
+    <label>Password</label>
+    <input type="password" class="form-control" aria-describedby="passwordHelp" name="password" placeholder="Mot de passe">
+    <small id="passwordHelp" class="form-text text-muted">Ne donner votre mot de passe à personne !</small>
+  </div>
+  <button type="submit" class="btn btn-primary">Connexion</button>
+</form>
