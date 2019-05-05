74f38601f9dff998953ee180a60185c50b3e0af5
Jonathan <super90xd1@gmail.com>
2019-05-05 19:38:31 +0200

    Change name of input

--- application/views/inscription-view.php
+++ application/views/inscription-view.php
@@ -3,33 +3,37 @@
     <h1 class="display-4">Inscription</h1>
   </div>
 </div>
-<form class="inscription" action="Inscription" class="inscription" method="post">
+<form class="inscription" action="Inscription" class="inscription" method="post"  enctype="multipart/form-data">
   <div class="form-group">
     <label>Pseudo</label>
-    <input type="text" class="form-control" name="pseudo" id="pseudo" placeholder="Pseudo">
+    <input type="text" class="form-control" name="pseudo" id="pseudo" placeholder="Pseudo" required>
   </div>
   <div class="form-row">
-    <div class="form-group col-md-6">
+    <div class="form-group col-md-4">
       <label>Nom</label>
       <input type="text" class="form-control" name="nom" id="nom" placeholder="Dubuis">
     </div>
-    <div class="form-group col-md-6">
+    <div class="form-group col-md-4">
       <label>prénom</label>
       <input type="text" class="form-control" name="prenom" id="prenom" placeholder="Guillaume">
+    </div>
+    <div class="form-group col-md-4">
+      <label>Date de naissance</label>
+      <input type="date" class="form-control" name="naissance" id="date">
     </div>
   </div>
   <div class="form-row">
     <div class="form-group col-md-6">
       <label>Genre</label>
-      <select class="custom-select mr-sm-2" id="inlineFormCustomSelect">
+      <select class="custom-select mr-sm-2" name="genre" id="inlineFormCustomSelect">
         <option value="1">Homme</option>
         <option value="2">Femme</option>
-        <option value="3">Non spécifié</option>
+        <option value="3" selected>Non spécifié</option>
       </select>
     </div>
     <div class="form-group col-md-6">
       <label>Pays</label>
-      <select class="custom-select mr-sm-2" id="inlineFormCustomSelect">
+      <select class="custom-select mr-sm-2" name="pays" id="inlineFormCustomSelect">
         <option value="1">Suisse</option>
         <option value="2">France</option>
       </select>
@@ -41,7 +45,7 @@
         <span class="input-group-text" id="inputGroupFileAddon01">Photo de profil</span>
       </div>
       <div class="custom-file">
-        <input type="file" class="custom-file-input" id="inputGroupFile01" aria-describedby="inputGroupFileAddon01">
+        <input type="file" class="custom-file-input" name="fichier" aria-describedby="inputGroupFileAddon01">
         <label class="custom-file-label" for="inputGroupFile01">Choisir un fichier</label>
       </div>
     </div>
@@ -49,11 +53,11 @@
   <div class="form-row">
     <div class="form-group col-md-6">
       <label>Mot de passe</label>
-      <input type="password" class="form-control" name="password1" id="password1">
+      <input type="password" class="form-control" name="password1" id="password1" required>
     </div>
     <div class="form-group col-md-6">
       <label>Confirmer mot de passe</label>
-      <input type="password" class="form-control" name="password2" id="password2">
+      <input type="password" class="form-control" name="password2" id="password2" required>
     </div>
   </div>
   <button type="submit" class="btn btn-outline-primary btn-lg btn-block" >S'inscrire</button>
