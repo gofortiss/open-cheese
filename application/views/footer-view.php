bca79ad3cde2c314c65cadaa3976d1dece36b33d
CPLN <super90xd1@gmail.com>
2019-05-09 12:13:51 +0200

    Add bootstrap-table

--- application/views/footer-view.php
+++ application/views/footer-view.php
@@ -1,5 +1,6 @@
     <div class="footer">
       <h5>Made with ♡ by Jonathan Othenin-Girard</h5>
     </div>
+    
   </body>
 </html>
--- application/views/header-view.php
+++ application/views/header-view.php
@@ -7,11 +7,16 @@
 		<link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
 		<link rel="stylesheet" href="<?php echo base_url('assets/css/style.css');?>">
 		<link rel="stylesheet" href="<?php echo base_url('assets/css/fluent.min.css');?>">
+		<script src = "https://code.jquery.com/jquery-3.3.1.min.js"></script>
 		<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
-		<link href="https://fonts.googleapis.com/css?family=Roboto+Mono" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+		<script src="https://unpkg.com/bootstrap-table@1.14.2/dist/bootstrap-table.min.js"></script>
 		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+		<script src="https://unpkg.com/bootstrap-table@1.14.2/dist/bootstrap-table.min.js"></script>
+		<link href="https://fonts.googleapis.com/css?family=Roboto+Mono" rel="stylesheet">
+		<link rel="stylesheet" href="https://unpkg.com/bootstrap-table@1.14.2/dist/bootstrap-table.min.css">
 		<title><?php echo $title; ?></title>
 		<!-- Afficher message javascript -->
 		<script type="text/javascript">
