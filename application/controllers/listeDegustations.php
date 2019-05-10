3b62a9b22d952f0ec52f2ee9c2efe85c4e26c8bd
CPLN <super90xd1@gmail.com>
2019-05-10 13:30:32 +0200

    Create controllers list degustation

--- application/controllers/listeDegustations.php
+++ application/controllers/listeDegustations.php
@@ -1,7 +1,11 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-class FormDegustationsListe extends CI_Controller {
-
+class listeDegustations extends CI_Controller {
+public function __construct(){
+  parent::__construct();
+  $this->load->model("degustations");
+  $this->load->model("functions");
+}
   public function index()
   {
     // Titre de la page
@@ -9,10 +13,16 @@ class FormDegustationsListe extends CI_Controller {
 
     // Chargement de la session
     $this->load->helper('url');
-    $this->load->model("functions");
     // Vérifie si l'utilisateur est déconnecté
     $this->load->view('header-view',$data); // Load header
     $this->load->view('degustations-liste-view');
     $this->load->view('footer-view');
+  }
+
+    public function api()
+  {
+      $degustations = $this->degustations->getDegustations();
+      $degustations = json_encode($degustations,true);
+      echo $degustations;
   }
 }
