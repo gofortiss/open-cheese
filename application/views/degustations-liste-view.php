63d58654cb25d9ab3e29a93e89658e5aeb6b6cc1
CPLN <super90xd1@gmail.com>
2019-05-10 13:31:02 +0200

    Create view list degustation

--- application/views/degustations-liste-view.php
+++ application/views/degustations-liste-view.php
@@ -1,24 +1,46 @@
+<div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-4">Liste des dégustations</h1>
+  </div>
+</div>
 <table id="table"></table>
 <script type="text/javascript">
-  $(document).ready(function(){
-    $('#table').bootstrapTable({
-      search: true,
-      columns: [ {
-        field: 'name',
-        title: 'Item Name'
-      }, {
-        field: 'price',
-        title: 'Item Price'
-      }],
-      data: [{
-
-        name: 'Item 1',
-        price: '$1'
-      }, {
-
-        name: 'Item 2',
-        price: '$2'
-      }]
-    })
-  });
+$(document).ready(function(){
+      // Affichage des dégustations
+      $('#table').bootstrapTable({
+        search : true,
+        url:"<?php echo base_url(); ?>index.php/listeDegustations/api",
+        pagination: true,
+        columns: [{
+          visible : false,
+          field: '0',
+          title: "Identifiant unique"
+        },{
+          field: 'dateAjout',
+          title: "Date de l'ajout"
+        }, {
+          field: 'nom',
+          title: 'Fromage'
+        },
+        {
+          field: 'type',
+          title: 'Type de pâte'
+        },
+        {
+          field: 'typeLait',
+          title: 'Type de lait'
+        },
+        {
+          field: 'pasteurise',
+          title: 'pasteurisé'
+        }, {
+          field: 'note',
+          title: 'Note de la dégustation'
+        }],
+        // Si une ligne est cliquée redirection sur la page de la dégustation
+        onClickRow: function (row, element, field) {
+          window.location.href = "<?php echo base_url(); ?>index.php/degustation?id="+row['0'];
+        }
+      })
+});
 </script>
