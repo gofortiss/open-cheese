dee33cad4e5eac33b5e6ccf4e67221de987afa5c
Jonathan <super90xd1@gmail.com>
2019-05-11 16:10:30 +0200

    Change list historique

---
+++ application/controllers/listeHistorique.php
@@ -0,0 +1,27 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+class listeHistorique extends CI_Controller {
+public function __construct(){
+  parent::__construct();
+  $this->load->model("historique");
+}
+  public function index()
+  {
+    // Titre de la page
+    $data['title'] = "Liste des dégustations";
+
+    // Chargement de la session
+    $this->load->helper('url');
+    // Vérifie si l'utilisateur est déconnecté
+    $this->load->view('header-view',$data); // Load header
+    $this->load->view('historique-liste-view');
+    $this->load->view('footer-view');
+  }
+
+    public function api()
+  {
+      $degustations = $this->historique->getAllDegustations();
+      $degustations = json_encode($degustations,true);
+      echo $degustations;
+  }
+}
