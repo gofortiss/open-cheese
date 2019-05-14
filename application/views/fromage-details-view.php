41d0c91d06f98ec0f870d02b6220cba69cfa1fcb
CPLN <super90xd1@gmail.com>
2019-05-14 11:43:11 +0200

    Re-design fromage-view

--- application/views/fromage-details-view.php
+++ application/views/fromage-details-view.php
@@ -5,10 +5,10 @@
   </div>
 </div>
 <div class="form-row degustation">
-  <div class="form-group col-lg-4 border">
+  <div class="form-group col-lg-6 border">
     <h2 class="titre">Le produit</h2>
     <h4>Description :</h4>
-    <p class="description-fromage"></p>
+    <p class="description-fromage" style="width: 400px;text-align:left;margin:auto"></p>
     <h4>Type de lait</h4>
     <p class="type-de-lait-fromage"></p>
     <h4>Type de pâte</h4>
@@ -16,44 +16,34 @@
     <h4>Est pasteurisé</h4>
     <p class="pasteurise-fromage"></p>
   </div>
-  <div class="form-group col-lg-4 border">
+  <div class="form-group col-lg-6 border">
     <h2 class="titre">Valeur énergetique</h2>
-  <table class="table">
-    <thead>
-      <tr>
-        <th scope="col">Calories</th>
-        <th scope="col">Proteines</th>
-        <th scope="col">Lipides</th>
-        <th scope="col">Sodium</th>
-      </tr>
-    </thead>
-  <tbody>
-    <tr>
-      <th class="proteines-fromage"></th>
-      <th class="lipides-fromage"></th>
-      <th class="calories-fromage"></th>
-      <th class="sodium-fromage"></th>
-    </tr>
-  </tbody>
-</table>
+      <table class="table">
+        <thead class="">
+          <tr>
+            <th scope="col">Calories</th>
+            <th scope="col">Proteines</th>
+            <th scope="col">Lipides</th>
+            <th scope="col">Sodium</th>
+          </tr>
+        </thead>
+      <tbody>
+        <tr>
+          <th class="proteines-fromage"></th>
+          <th class="lipides-fromage"></th>
+          <th class="calories-fromage"></th>
+          <th class="sodium-fromage"></th>
+        </tr>
+      </tbody>
+    </table>
+    <h2 class="titre">Dégustation</h2>
+    <a class="btn btn-primary btn-lg ajouterDegustation" href="<?php echo base_url(); ?>index.php/degustation/ajouterDegustation/?id=" class="btn btn-primary btn-lg">Ajouter une dégustation</a>
   </div>
-  <div class="form-group col-lg-4 border">
-    <h2 class="titre">Note des utilisateurs</h2>
-    <h2 class="titre">4/5</h2>
-    <h2 class="titre">Nombre d'avis</h2>
-    <h2 class="titre">18</h2>
-  </div>
-</div>
-<div class="form-row degustation">
-  <div class="form-group col-lg-12 border">
-      <h2 class="titre">Commentaires</h2>
-  </div>
-</div>
 <script type="text/javascript">
   $(document).ready(function(){
     $.ajax({
           type: "POST",
-          url: "<?php echo base_url(); ?>index.php/fromage/api?id=<?php echo $id; ?>",
+          url: "<?php echo base_url(); ?>index.php/fromage/apiFromage?id=<?php echo $id; ?>",
           dataType: "json",
           success: function (data) {
             console.log(data);
@@ -68,6 +58,8 @@
             $('.lipides-fromage').text(data[0]['lipides']);
             $('.proteines-fromage').text(data[0]['proteines']);
             $('.sodium-fromage').text(data[0]['sodium']);
+
+            $('.ajouterDegustation').attr("href","<?php echo base_url(); ?>index.php/degustation/ajouterDegustation/?id="+data[0]['numero']);
           }
       });
 
