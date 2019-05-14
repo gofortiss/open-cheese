bba69e1499bc6a1e4bcbc9157dba7da81738beaf
Jonathan <super90xd1@gmail.com>
2019-05-14 22:21:41 +0200

    Transform SQL query to codeigniter query

--- application/models/fromageAction.php
+++ application/models/fromageAction.php
@@ -17,21 +17,12 @@ class fromageAction extends CI_Model
 
   public function getFromage($id)
   {
-    $cnn = getConnexion('open-cheese');
-    $stmt = $cnn->prepare('SELECT * FROM `tblfromage` INNER JOIN tbltypepate ON num_tbltypePate = tbltypepate.numero
-    INNER JOIN tbllait ON num_tblLait = tbllait.numero
-    INNER JOIN tblpasteurise ON num_tblpasteurise = tblpasteurise.numero
-    WHERE tblfromage.numero = :numero');
-    $stmt->bindValue(':numero',$id);
-    $stmt->execute();
-    $result = $stmt->fetchAll();
-    return $result;
-
     $this->db->select("tblfromage.numero as fromage_numero, nom,description_fromage,calories,proteines,lipides,sodium,type,typeLait,pasteurise,photo_fromage");
     $this->db->from('tblfromage');
     $this->db->join('tbltypepate','tblfromage.num_tbltypePate=tbltypepate.numero');
     $this->db->join('tbllait','tblfromage.num_tbllait=tbllait.numero');
     $this->db->join('tblpasteurise','tblfromage.num_tblpasteurise=tblpasteurise.numero');
+    $this->db->where('tblfromage.numero', $id);
     $query=$this->db->get();
     $data=$query->result();
     return $data;
