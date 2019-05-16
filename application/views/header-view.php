c4a193415153c722043c6b268ddbae9a1a3ba1bb
CPLN <super90xd1@gmail.com>
2019-05-16 12:08:41 +0200

    Add material icon

--- application/views/header-view.php
+++ application/views/header-view.php
@@ -9,6 +9,7 @@
 		<link rel="stylesheet" href="<?php echo base_url('assets/css/fluent.min.css');?>">
 		<script src = "https://code.jquery.com/jquery-3.3.1.min.js"></script>
 		<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+		<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
 		<script src="https://unpkg.com/bootstrap-table@1.14.2/dist/bootstrap-table.min.js"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
