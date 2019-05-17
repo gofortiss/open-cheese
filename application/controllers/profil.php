78ec8fbaa3757f391f55924b9a73e6dbfb12af4c
CPLN <super90xd1@gmail.com>
2019-05-17 11:08:25 +0200

    Add new friend developpement

--- application/controllers/profil.php
+++ application/controllers/profil.php
@@ -3,11 +3,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class profil extends CI_Controller {
 public function __construct(){
   parent::__construct();
+  $this->load->model("friendAction");
+  $this->load->database();
   $this->load->model("userAction");
   $this->load->helper('url');
 }
   public function index()
   {
+    // Vérification du paramètre d'url
     if (isset($_GET['id'])) {
       // Titre de la page
       $data['title'] = "Profil";
@@ -20,6 +23,19 @@ public function __construct(){
     }
     else{
         header('Location:'.base_url('index.php/fromage/listeFromage'));
+    }
+  }
+
+  public function newFriend()
+  {
+    // Vérification du paramètre d'url et que l'utilisateur soit connecté
+    if(isset($_GET['id']) && isset($_SESSION['idUser'])) {
+      $liste = $this->friendAction->getAllRelation(); // Liste de toutes les relations
+      $relation = $this->friendAction->getRelation($liste); // Liste des relations de l'utilisateur connecté
+      $this->friendAction->newRelation($_GET['id'], $relation); // Liste des relations de l'utilisateur connecté
+      
+    } else {
+      header('Location:'.base_url('index.php/fromage/listeFromage'));
     }
   }
 
