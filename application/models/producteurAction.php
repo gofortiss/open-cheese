eb7ceaac2acb6db60f1b4d768827e00f7066795b
CPLN <super90xd1@gmail.com>
2019-05-17 14:28:11 +0200

    Add producteur function

--- application/models/producteurAction.php
+++ application/models/producteurAction.php
@@ -50,4 +50,40 @@ class producteurAction extends CI_Model
     }
     $this->db->insert('tblfromage', $post);
   }
+
+  public function addProducteur($post, $file)
+  {
+    // Initialisation du message
+    $response = new Response();
+
+    // Vérification si un fichier à été envoyé
+    if($file['fichier']['name']!='')
+    {
+      $image = uploadImage($file,'producteur/'); // Upload de l'image du fromage
+      $post['photo_producteur'] = $image['nomFichier'];
+
+    } else {
+      $post['photo_producteur'] = 'default.png';
+    }
+
+    // Vérification du canton
+    if($post['num_tblpays'] != '1') {  // Si le pays n'est pas Suisse
+      if($post['num_tblcanton'] != '1') { // Et un canton à été choisi
+        $response->addMessage("canton"); // Message d'erreur du canton
+      }
+    }
+
+
+    // Si aucune erreur n'est survenue jusque là
+    if (empty($response->message)) {
+      $this->db->insert('tblproducteur', $post); // Insertion des données
+      // Si le nom du producteur existe déjà (erreur 1062 = duplicata de donnée unique)
+      if ($this->db->_error_number()==1062) {
+          $response->addMessage("nom"); // Message de succès
+        } else {
+          $response->addMessage("success"); // Message de succès
+        }
+    }
+    return $response->info(); // Envoi du message au controlleur
+  }
 }
