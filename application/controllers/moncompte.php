bae33ce16a123bb0a9873fd583d97ca48f9fb624
Jonathan <super90xd1@gmail.com>
2019-05-19 19:10:04 +0200

    Load db

--- application/controllers/moncompte.php
+++ application/controllers/moncompte.php
@@ -4,6 +4,7 @@ class moncompte extends CI_Controller {
   public function __construct(){
     parent::__construct();
     $this->load->model("userAction");
+    $this->load->database();
     $this->load->helper('url');
   }
 
