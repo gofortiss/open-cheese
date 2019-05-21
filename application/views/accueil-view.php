7abe92ea7475daf986afad7f40cb07b90157e051
CPLN <super90xd1@gmail.com>
2019-05-21 11:36:47 +0200

    Add content to home-page

--- application/views/accueil-view.php
+++ application/views/accueil-view.php
@@ -4,18 +4,29 @@
 </div>
 
 <div class="container">
-  <div class="form-row">
+  <div class="form-row home-card-container">
       <div class="form-group col-lg-4">
         <div class="home-card">
-          <img src="<?php echo base_url('assets/images/logo/fromages.png') ?>" class="card-img-top" alt="...">
+          <img src="<?php echo base_url('assets/images/logo/fromage.png') ?>" class="card-img-top" alt="frromage">
           <h5 class="card-title">Les fromages</h5>
           <p class="card-text">Venez découvrir les fromages du monde entier !</p>
-          <button type="button" class="btn btn-outline-primary btn-block">Voir les fromages</button>
+          <a href="<?php echo base_url('index.php/fromage/listeFromage')?>" class="btn btn-outline-primary btn-block">Voir les fromages</a>
         </div>
       </div>
       <div class="form-group col-lg-4">
         <div class="home-card">
-          <img src="<?php echo base_url('assets/images/logo/fromages.png') ?>" class="card-img-top" alt="...">
+          <img src="<?php echo base_url('assets/images/logo/fromages.png') ?>" class="card-img-top" alt="dégustation">
+          <h5 class="card-title">Les dégustations</h5>
+          <p class="card-text">Visualisez les dégustations de la communautés</p>
+          <a href="<?php echo base_url('index.php/listeHistorique')?>" class="btn btn-outline-primary btn-block">Voir les dégustations</a>
+        </div>
+      </div>
+      <div class="form-group col-lg-4">
+        <div class="home-card">
+          <img src="<?php echo base_url('assets/images/logo/worker.png') ?>" class="card-img-top" alt="dégustation">
+          <h5 class="card-title">Vous êtes producteur ?</h5>
+          <p class="card-text">Ajouter votre entreprise au site web, pour avoir une meilleure visibilité</p>
+          <a href="<?php echo base_url('index.php/fromage/ajouterProducteur')?>" class="btn btn-outline-primary btn-block">Ajouter un producteur</a>
         </div>
       </div>
       <div class="form-group col-lg-4">
@@ -23,4 +34,40 @@
         </div>
       </div>
   </div>
+</div>
+
+<div class="jumbotron" style="margin-bottom: 0px;">
+  <h1 class="display-4">Notez les meilleurs formage du monde</h1>
+  <hr class="my-4">
+   <p class="lead">Open-cheese offre la possibilitée à n'importe qui de pouvoir noter les fromages les plus préstigieux</p>
+</div>
+
+<div class="jumbotron" style="background-color:#ffc1073d;margin-bottom: 0px;">
+  <h1 class="display-4">Pourquoi open cheese ?</h1>
+    <div class="container" style="margin-top:25px;">
+      <div class="form-row">
+          <div class="form-group col-lg-4">
+            <div class="home-card">
+              <img src="<?php echo base_url('assets/images/logo/group.png') ?>" class="card-img-top" alt="frromage">
+              <h5 class="card-title">Grande communauté</h5>
+            </div>
+          </div>
+          <div class="form-group col-lg-4">
+            <div class="home-card">
+              <img src="<?php echo base_url('assets/images/logo/site.png') ?>" class="card-img-top" alt="dégustation">
+              <h5 class="card-title">Site web professionel</h5>
+            </div>
+          </div>
+          <div class="form-group col-lg-4">
+            <div class="home-card">
+              <img src="<?php echo base_url('assets/images/logo/diploma.png') ?>" class="card-img-top" alt="dégustation">
+              <h5 class="card-title">Producteur certifé</h5>
+            </div>
+          </div>
+          <div class="form-group col-lg-4">
+            <div class="home-card">
+            </div>
+          </div>
+      </div>
+    </div>
 </div>
